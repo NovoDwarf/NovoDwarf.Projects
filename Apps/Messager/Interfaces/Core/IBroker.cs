052f30a554db861d316dde37dd97d02c889f9025
d1rknwh1te3 <kirillstarodub310@gmail.com>
2025-11-11 14:34:36 +0300

    Remove Messager

--- Apps/Messager/Interfaces/Core/IBroker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Messager.Interfaces.Core;
-
-public interface IBroker<TEvent>
-{
-	public Guid Id { get; internal set; }
-	
-	public string BrokerType { get; }
-	public string EventType { get; }
-}
