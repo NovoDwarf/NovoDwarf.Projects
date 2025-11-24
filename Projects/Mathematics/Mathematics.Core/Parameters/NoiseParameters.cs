eb0514da75e992d5bccdc150bfc4b6c1a980a695
d1rknwh1te3 <kirillstarodub310@gmail.com>
2025-11-25 01:42:35 +0300

    Remove useless [Parameters]

--- Projects/Mathematics/Mathematics.Core/Parameters/NoiseParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mathematics.Core.Parameters;
-
-public record NoiseParameters
-{
-	[Range(0, 1)]
-	public float Scale { get; init; } = 0.01f;
-}
