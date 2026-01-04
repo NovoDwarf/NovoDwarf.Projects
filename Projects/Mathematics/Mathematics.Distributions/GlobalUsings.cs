a641298bd55090e5466776fb02361c3fd08863f6
d1rknwh1te3 <kirillstarodub310@gmail.com>
2026-01-04 12:36:25 +0300

    Add GlobalUsings.cs to Mathematics.Distirbutions

---
+++ Projects/Mathematics/Mathematics.Distributions/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using Mathematics.Core.Attributes;
+global using Mathematics.Core.Base.Entities;
+
+global using Float = Mathematics.Core.Utilities.FloatUtils; 
